a185e2fe2dac671b74c3d234bbf03779c989e4fc
Max R <Max.Roese@googlemail.com>
2022-07-19 14:35:03 +0200

    changes to index.js && index.html && style.css

--- index.js
+++ index.js
@@ -10,45 +10,45 @@ function run() {
         case 1: document.querySelector(".wuerfel").classList = "fa-solid fa-dice-one fa-4x wuerfel niete";
         const music2 = new Audio('sounds/mixkit-losing-bleeps-2026.wav');
         music2.play();
-        music2.volume = 0.2;
+        music2.volume = 0.1;
         music2.loop = false;
-        music2.playbackRate = 2;
+        music2.playbackRate = 3;
         music2.pause; 
         break;
 
         case 2: document.querySelector(".wuerfel").classList = "fa-solid fa-dice-two fa-4x wuerfel niete"; 
         const music1 = new Audio('sounds/mixkit-losing-bleeps-2026.wav');
         music1.play();
-        music1.volume = 0.2;
+        music1.volume = 0.1;
         music1.loop = false;
-        music1.playbackRate = 2;
+        music1.playbackRate = 3;
         music1.pause;
         break;
 
         case 3: document.querySelector(".wuerfel").classList = "fa-solid fa-dice-three fa-4x wuerfel niete";
         const music3 = new Audio('sounds/mixkit-losing-bleeps-2026.wav');
         music3.play();
-        music3.volume = 0.2;
+        music3.volume = 0.1;
         music3.loop = false;
-        music3.playbackRate = 2;
+        music3.playbackRate = 3;
         music3.pause; 
         break;
 
         case 4: document.querySelector(".wuerfel").classList = "fa-solid fa-dice-four fa-4x wuerfel niete"; 
         const music4 = new Audio('sounds/mixkit-losing-bleeps-2026.wav');
         music4.play();
-        music4.volume = 0.2;
+        music4.volume = 0.1;
         music4.loop = false;
-        music4.playbackRate = 2;
+        music4.playbackRate = 3;
         music4.pause;
         break;
 
         case 5: document.querySelector(".wuerfel").classList = "fa-solid fa-dice-five fa-4x wuerfel niete";
         const music5 = new Audio('sounds/mixkit-losing-bleeps-2026.wav');
         music5.play();
-        music5.volume = 0.2;
+        music5.volume = 0.1;
         music5.loop = false;
-        music5.playbackRate = 2;
+        music5.playbackRate = 3;
         music5.pause; 
         break;
 
@@ -56,16 +56,21 @@ function run() {
         const music = new Audio('sounds/mixkit-retro-game-notification-212.wav');
         music.play();
         music.loop = false;
-        music.volume = 0.2;
-        music.playbackRate = 2;
+        music.volume = 0.1;
+        music.playbackRate = 3;
         music.pause;
         break;
     }
 
     if (dice == 6) {
         kontostand += 5;
+        document.querySelector("#kontostand").classList = "kontostand green";
+          
+          delay(1000).then(() => document.querySelector("#kontostand").classList = "");
     } else {
         kontostand -= 1;
+        document.querySelector("#kontostand").classList = "kontostand red";
+          delay(1000).then(() => document.querySelector("#kontostand").classList = ""); 
     };
 
     document.querySelector("#kontostand").innerHTML = kontostand + "€";
@@ -75,3 +80,7 @@ function reset() {
     kontostand = 100;
     document.querySelector("#kontostand").innerHTML = kontostand + "€";
 }; 
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
